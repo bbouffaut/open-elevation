bd7240a27c5523a11580b57e8d8ad9ea60ae8ae7
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2023-10-20 00:56:03 +0200

    Implement arm64 deployment

--- docker/build_and_deploy.sh
+++ docker/build_and_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 TAG=$1
+PLATFORM=$2
 
-./docker/build_arm_docker.sh $TAG
-/usr/bin/docker push registry.gitlab.com/skails/open-elevation-api:$TAG
+./docker/build_arm_docker.sh $TAG $PLATFORM
+docker push registry.gitlab.com/skails/open-elevation-api:$TAG
--- docker/build_arm_docker.sh
+++ docker/build_arm_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 TAG=$1
+PLATFORM=$2
 
-/usr/bin/docker buildx build --platform linux/arm/v7 -t registry.gitlab.com/skails/open-elevation-api:$TAG -f ./docker/Dockerfile .
+docker buildx build --platform $PLATFORM -t registry.gitlab.com/skails/open-elevation-api:$TAG -f ./docker/Dockerfile .
